09c6629382be1f0d6349662a290525d5b21e1338
clr.amestoy@gmail.com <clr.amestoy@gmail.com>
2023-04-30 20:57:02 -0300

    cloudflare

--- .vercel/output/functions/render.func/src/pages/posts/personalidad de las ratas.md
+++
@@ -1,8 +0,0 @@
----
-title: "Personalidad y caracter"
-description: "Todas las ratas no son iguales pero comparten algunas caracteristicas"
-pubDate: "2021-06-08"
-hero: "/images/personalidad.jpg"
-tags: ["adoptar"]
-layout: "../../layouts/Layout.astro"
----
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,19 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-import vercel from '@astrojs/vercel/serverless';
 
-
-
-
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ratfriendly.org",
-  integrations: [react(), tailwind(), alpinejs(), sitemap()],
+  integrations: [react(), tailwind(), alpinejs(), sitemap(), cloudflare()],
   output: 'server',
-  adapter: vercel(),
-  
+  adapter: cloudflare(),
   build: {
+    
     rollupOptions: {
       external: ['@astrojs/rss']
     }
-  },
+  }
 });
-
